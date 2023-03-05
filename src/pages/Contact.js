ca4040c7e23e9e150083cc7be3feac7657af0d1e
helena <helena.gilja@gmail.com>
2023-03-05 19:08:44 +0000

    contact page, mock links added, work in progress

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,14 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faLinkedin,
+  faGithub,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Contact() {
   return (
@@ -8,6 +17,40 @@ function Contact() {
         <Col lg="8">
           <h1 className="display-4 mb-4">Contact</h1>
           <hr className="t_border my-4 ml-0 text-left" />
+        </Col>
+      </Row>
+
+      <Row>
+        <h4>hey hey hey </h4>
+      </Row>
+
+      <Row>
+        <Col>
+          <a
+            href="https://www.linkedin.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="6x" />
+          </a>
+        </Col>
+        <Col>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} size="6x" />
+          </a>
+        </Col>
+        <Col>
+          <a
+            href="https://twitter.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faEnvelope} size="6x" />
+          </a>
         </Col>
       </Row>
     </Container>
